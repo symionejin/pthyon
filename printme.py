75473e42a5cc8dde020f45d73790e35c121cb36b
Symione King <symioneking@SymioneKingdeMacBook-Pro.local>
2016-10-13 23:58:36 +0800

    printme.py

---
+++ printme.py
@@ -0,0 +1,19 @@
+
+
+ 
+   #返回2个参数的和."
+   print( "函数内是局部变量 : ", total)
+   return total;
+   total = arg1 + arg2; # total在这里是局部变量.
+# 可写函数说明
+# 调用sum函数
+#调用sum函数
+def sum( arg1, arg2 ):
+li=[{"age":20,"name":"def"},{"age":25,"name":"abc"},{"age":10,"name":"ghi"}]
+li=sorted(li,key=lambda x:x["age"])
+print ("函数外是全局变量 : ", total )
+print ("相加后的值为 : ", sum( 20, 20 ))
+print(li)
+sum = lambda arg1, arg2: arg1 * arg2;
+sum( 10, 20 );
+total = 0; # 这是一个全局变量
